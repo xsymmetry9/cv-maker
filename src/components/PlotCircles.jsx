ea67583c9e2f281abf1971579f6d9fd8053799a6
Gary <glswim@gmail.com>
2024-01-02 09:13:31 +0800

    Changed from circles to stars

--- src/components/PlotCircles.jsx
+++ src/components/PlotCircles.jsx
@@ -2,7 +2,7 @@
 /* eslint-disable no-unused-vars */
 import React from 'react'
 
-const PlotCircles = ({color, level}) =>{
+const PlotCircles = ({level}) =>{
     const circleArray = Array.from({ length: 5 }, (_, i) => i < level);
     // const arr = new Array(5).fill(false);
     // for(let i = 0; i < level; i++)
@@ -13,13 +13,17 @@ const PlotCircles = ({color, level}) =>{
     // console.log(arr);
 
     return(
-            <div className='flex gap-2 items-center'>
+            <div className='grid grid-cols-5 gap-1 items-center'>
                 {circleArray.map((item, index)=> (
                     <div
                         key={index}
-                        style={{background: item ? color : 'gray'}}
-                        className='h-[8px] w-[8px] rounded-full'></div>
-                    ))}
+                        // style={{borderColor: item ? "lightgray" : 'gray'}}
+                        className='flex justify-center items-center h-[18px] w-[18px] rounded-full border'>
+                            {item && <i className='fa-solid fa-star fa-xs text-yellow-400'></i>}
+                        </div>
+                    )
+                )
+                }
             </div>
     );
 };
