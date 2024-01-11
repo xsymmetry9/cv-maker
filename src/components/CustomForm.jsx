a18c50c271d9030e32e1db9e4076f5f468df725b
Gary <glswim@gmail.com>
2024-01-11 23:37:33 +0800

    fix selected element

--- src/components/CustomForm.jsx
+++ src/components/CustomForm.jsx
@@ -3,17 +3,15 @@
 import React from 'react';
 
 const CustomForm =({layout, handle, backgroundColor}) =>{
-    const style = `w-[80px] h-[90px] py-4 border-slate-300 rounded-md`;
+    const style = `w-[80px] h-[90px] py-4 rounded-md`;
     const hover = `hover:border-slate-700 hover:border-2 `;
-
-    console.log(layout);
     return(
         <div className='style-card mt-12 bg-stone-100 shadow shadow-sm shadow-stone-700/50 rounded-md'>
            <h2 className="font-bold text-center text-2xl pb-7 pt-3">Outline</h2>
             <div className="group-buttons flex gap-3 justify-center">
                 {layout.map((item) =>{
                     return(
-                        <button key={item.name} className={`${item.isSelected ? "border-2 border-red-700" : "border border-slate-300"} ${style} ${hover}`}
+                        <button key={item.name} className={`${item.isSelected ? "border-2 border-slate-700" : "border border-slate-300"} ${style} ${hover}`}
                         style={{background: `linear-gradient(to ${item.name}, #fff 0 70%, ${backgroundColor} 70% 100%)`}}
                         name={item.name}
                         onClick={handle}>
