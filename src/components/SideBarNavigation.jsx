bd649dc02a613be78c8d4d28e3982f27d89e624f
Gary <glswim@gmail.com>
2024-01-12 09:02:50 +0800

    Changed background color

--- src/components/SideBarNavigation.jsx
+++ src/components/SideBarNavigation.jsx
@@ -19,14 +19,14 @@ const SideBarNavigation = ({data, handle}) =>{
 
     const Button = ({name, handle}) =>{
         return(
-            <button className='flex gap-3 items-center px-3 border-y border-stone-500 hover:bg-stone-500 h-[150px]' name={name} onClick= {handle}>
+            <button className='flex gap-3 items-center px-3 py-1 hover:bg-stone-500' name={name} onClick= {handle}>
                 <i className={`fa-solid ${getIcon(name)} text-slate-50`}></i>
-                <span className='font-bold text-slate-50'>{name.toUpperCase()}</span>
+                <span className='font-bold font-mono text-slate-50'>{name.toUpperCase()}</span>
             </button>
         );
     };
     return(
-        <div className="flex flex-col gap-9">
+        <div className="flex flex-col divide-y divide-stone-300 border-b border-stone-300">
             {data.map((item, index) =>{ 
                 return(
                     <Button key={index} name ={item} handle={handle}/>)}
