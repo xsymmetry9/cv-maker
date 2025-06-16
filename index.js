72058602653f1260c609271bc5dfff5f33bef6b6
xsymmetry9 <94920666+xsymmetry9@users.noreply.github.com>
2025-06-16 22:40:17 +0800

    Implement live preview functionality using DOM manipulation

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
-const plot = () => {
-    const container = document.getElementById("content");
+const getForm = document.querySelector(".form-container");
+const nameInput = document.getElementById("name");
+const emailInput = document.getElementById("email");
+const summaryInput = document.getElementById("summary");
 
+const nameHandler = (e) => {
+    const previewName = document.getElementById("preview-name");
+    previewName.textContent= e.currentTarget.value;
+}
+
+const plot = () => {
     const button = document.createElement("button");
     button.textContent = "Submit"
-    container.appendChild(button);
+    getForm.appendChild(button);
 }
+
+const plotPreview = () => {
+
+
+    
+}
+
+nameInput.addEventListener("input", nameHandler);
 
 plot();
