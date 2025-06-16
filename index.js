fa4177166c3c1b1491e941ede6e43fcdfda9ba3c
xsymmetry9 <94920666+xsymmetry9@users.noreply.github.com>
2025-06-16 21:26:23 +0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+const plot = () => {
+    const container = document.getElementById("content");
+
+    const heading = document.createElement("h1");
+    heading.textContent = "Hello"
+    container.appendChild(heading);
+}
+
+plot();
